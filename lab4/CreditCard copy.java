819799bc3c09e93d8bd90e9ba2dcea4889035f49
JoseGordilloMendoza <jgordillome@unsa.edu.pe>
2025-05-13 10:29:04 -0500

    probando

--- lab4/CreditCard copy.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface CreditCard extends Remote {
-    void charge(double amount) throws RemoteException;  // carga
-    double getBalance() throws RemoteException;  // obtener saldo
-    void setBalance(double balance) throws RemoteException;  // setear saldo
-}
